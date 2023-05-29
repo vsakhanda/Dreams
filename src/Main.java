667bad34c15b4d7cf3b7705c5fc0332d42f36554
vsakhanda <vsakhanda@gmail.com>
2023-05-29 23:34:57 +0300

    Я починаю курс Java в robot_dreams і вже вмію використовувати Git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println(" Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
         System.out.println("Hello world! Happy new year!");
     }
 }
