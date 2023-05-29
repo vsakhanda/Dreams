a8e199836f6bd9803dd5a425c0fe314be1f775e7
vsakhanda <vsakhanda@gmail.com>
2023-05-29 23:07:19 +0300

    new comment for commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world! Happy new year!");
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+
+//new raw in second branch
     }
 }
