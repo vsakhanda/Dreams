64b733e901c3dd82747c2fba92414455a8680670
vsakhanda <vsakhanda@gmail.com>
2023-06-03 18:25:23 +0300

    ThirdLecture lecture

---
+++ src/Thrird/ThirdLecture.java
@@ -0,0 +1,20 @@
+package Thrird;
+
+public class ThirdLecture {
+
+    public static void main(String[] args)
+    {
+        boolean booleanVariable = true ;// true false
+        byte byteVariable = 127 ; // -128 -- 127
+        // Unicode Уніфіковане кодування
+
+
+        System.out.println(booleanVariable);
+        System.out.println(byteVariable);
+    }
+
+
+
+    }
+
+
