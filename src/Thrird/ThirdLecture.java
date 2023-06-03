f41b071326c813fa86638202c6280c721b6161a5
vsakhanda <vsakhanda@gmail.com>
2023-06-03 19:10:09 +0300

    new comment for commit

--- src/Thrird/ThirdLecture.java
+++ src/Thrird/ThirdLecture.java
@@ -7,10 +7,32 @@ public class ThirdLecture {
         boolean booleanVariable = true ;// true false
         byte byteVariable = 127 ; // -128 -- 127
         // Unicode Уніфіковане кодування
+        char charVariable = '%';
+        short shortVariable = 150; //-32.768 to 32.767
+        int intVariable = 32321212; // 2^32
+        long longVariable = 321321321321L; //2^62
+        float floatVariable = 321.32f; // 2^32
+        double doubleVariable = 123123132.67f;//2^62
 
+
+
+        String y = Character.toString( 128_512);
+        System.out.println(y);
 
         System.out.println(booleanVariable);
         System.out.println(byteVariable);
+
+        System.out.println(shortVariable);
+        System.out.println(longVariable);
+
+        System.out.println(floatVariable);
+        System.out.println(doubleVariable);
+
+
+
+        //RAM - check wiki
+
+
     }
 
 
