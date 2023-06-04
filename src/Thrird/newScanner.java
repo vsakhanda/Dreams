89580d0035200ceddf32837572887e1628648af2
vsakhanda <vsakhanda@gmail.com>
2023-06-04 09:45:47 +0300

    Scanner creation on Windows

--- src/Thrird/newScanner.java
+++ src/Thrird/newScanner.java
@@ -1,13 +1,9 @@
 package Thrird;
 
-/**
- *  Example of use scanner in java. It made input of objects in console ad use them in program work.
- *
- */
 import java.util.Scanner;
 
 
-public class Scanner {
+public class newScanner {
 
     public static void main(String[] args)
     {
@@ -15,7 +11,7 @@ public class Scanner {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter user name");
 
-        String userName = myObj.nextline();
+        String userName = myObj.nextLine();
         System.out.println ("Username is "+ userName);
 
     }
