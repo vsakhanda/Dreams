ff7b8e9cbdd4625d46c8e44499a55c08fd5a6f83
vsakhanda <vsakhanda@gmail.com>
2023-06-04 21:59:51 +0300

    Доповнення домашнього завдання

--- src/Fourth/FourthHomeWork.java
+++ src/Fourth/FourthHomeWork.java
@@ -1,4 +1,7 @@
 package Fourth;
+
+import java.util.Scanner;
+
 /**
  *Створити програму для обробки символьних рядків:
  * Створити зміну типу String
@@ -15,6 +18,21 @@ public class FourthHomeWork {
         System.out.println(test.toLowerCase());
         System.out.println(test.charAt(0));
         System.out.println(test.charAt(test.length()-1));
+
+        /*
+        Додатково виткористав метод Scanner для обробки рядка, який вводить користувач
+         */
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Напишіть стрічку для обробки");
+
+        String someScanner = myObj.nextLine();
+        System.out.println("Довжина рядка = "+ someScanner.length());
+        System.out.println("Рядок прописом = "+ someScanner.toUpperCase());
+        System.out.println("Рядок в нижньому регістрі  = "+ someScanner.toLowerCase());
+        System.out.println("Перша літера рядка = "+ someScanner.charAt(0));
+        System.out.println("Остання літера рядка" + someScanner.charAt(test.length()-1));
+
 
 
     }
--- src/Fourth/FourthLecture.java
+++ src/Fourth/FourthLecture.java
@@ -1,5 +1,7 @@
 package Fourth;
 
+import java.util.Scanner;
+
 public class FourthLecture {
         public static void main(String[] args)    {
             FourthLecture fourthLecture = new FourthLecture();
@@ -18,6 +20,25 @@ public class FourthLecture {
         System.out.println(test.length());
         System.out.println(test.concat("Upper").concat("Eleven"));
         System.out.println(test.toUpperCase());
+
+/*
+Метод створений для введеня тексту та його обробки
+
+ */
+
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter стрічку для обробки");
+
+            String someScanner = myObj.nextLine();
+            System.out.println("Довжина рядка = "+ someScanner.length());
+            System.out.println("Рядок прописом = "+ someScanner.toUpperCase());
+            System.out.println("Рядок в нижньому регістрі  = "+ someScanner.toLowerCase());
+            System.out.println("Перша літера рядка = "+ someScanner.charAt(0));
+            System.out.println("Остання літера рядка" + someScanner.charAt(test.length()-1));
+
+
+
+
         }
     private int sum(int a, int b) {
         return a+b;
