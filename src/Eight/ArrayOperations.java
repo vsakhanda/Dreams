9dea2bc1a788b775dfa789a5881d9f5bf680e747
Odyssey <vsakhanda@gmail.com>
2023-06-22 23:10:38 +0300

    eight lecture

---
+++ src/Eight/ArrayOperations.java
@@ -0,0 +1,10 @@
+package Eight;
+
+public class ArrayOperations {
+
+    public static void operations(String[] args) {
+        // Введення даних про дві матриці
+        System.out.println("start");
+
+    }
+}
---
+++ src/Eight/EightLecture.java
@@ -0,0 +1,15 @@
+package Eight;
+
+public class EightLecture {
+
+    public static void main(String[] args) {
+        String[] mainArray = new String[5];
+        System.out.println(mainArray.length);
+
+    };
+
+
+
+
+
+}
