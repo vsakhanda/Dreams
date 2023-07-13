b098eb94bc6c8d4224e3dc1c63f5c510ef67285a
Odyssey <vsakhanda@gmail.com>
2023-07-13 19:38:02 +0300

    HomeWork13

--- src/Thirteen/HomeWork/FirstYearStud.java
+++ src/Thirteen/HomeWork/FirstYearStud.java
@@ -2,18 +2,9 @@ package Thirteen.HomeWork;
 
 public class FirstYearStud extends Student{
 
-
-
-
-    @Override
-    public void score(){
-    };
-
-
     @Override
     public void info(){
         System.out.println("Student of first year");
         System.out.println("   Student name: " + getName() + " " + getSurname() +",  "+ getAge() + "year old");
-
-    };
+    }
 }
--- src/Thirteen/HomeWork/SecondYearStud.java
+++ src/Thirteen/HomeWork/SecondYearStud.java
@@ -3,15 +3,9 @@ package Thirteen.HomeWork;
 public class SecondYearStud extends Student  {
 
     @Override
-    public void score(){
-
-    };
-
-
-    @Override
     public void info(){
         System.out.println("Student of Second year");
         System.out.println("   Student name: " + getName() + " " + getSurname() +",  "+ getAge() + "year old");
-    };
+    }
 }
 
