a1c89ea78bf3018b4bf653d1867d2c1b37280846
Odyssey <vsakhanda@gmail.com>
2023-09-11 22:05:29 +0300

    homeWork30_1

--- src/l30Threads2/NewThread/sync/SyncLecture.java
+++ src/l30Threads2/NewThread/sync/SyncLecture.java
@@ -1,7 +1,9 @@
 package l30Threads2.NewThread.sync;
 
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 /*
 Ці утиліти допомагають координувати роботу між потоками,
@@ -33,5 +35,32 @@ public class SyncLecture {
         }
         System.out.println("Both tasks are done");
     }
+
+    private static void cyclicBarrier(){
+        CyclicBarrier barrier = new CyclicBarrier(2, () -> {
+            System.out.println("Both tasks reached the barrier");
+        });
+
+        new Thread(() -> {
+            System.out.println("Task 1 is approaching the barrier");
+            try {
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Task 1 passed the barrier");
+        }).start();
+
+        new Thread(() -> {
+            System.out.println("Task 2 is approaching the barrier");
+            try {
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Task 2 passed the barrier");
+        }).start();
+    }
+
 
 }
