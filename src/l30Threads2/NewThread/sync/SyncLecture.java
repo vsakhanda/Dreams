9d43277bb1120ef13f3fa5eebdfdd580a8ddd3a6
Odyssey <vsakhanda@gmail.com>
2023-09-12 10:41:41 +0300

    homeWork30_1

--- src/l30Threads2/NewThread/sync/SyncLecture.java
+++ src/l30Threads2/NewThread/sync/SyncLecture.java
@@ -4,6 +4,7 @@ package l30Threads2.NewThread.sync;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 /*
 Ці утиліти допомагають координувати роботу між потоками,
@@ -13,11 +14,13 @@ public class SyncLecture {
 
     public static void main(String[] args) {
         countDownLatch();
+        cyclicBarrier();
+        semaphore();
     }
 
     private static void countDownLatch() {
         CountDownLatch latch = new CountDownLatch(2);
-
+        System.out.println("Count down start");
         new Thread(() -> {
             System.out.println("Task 1 is running");
             latch.countDown();
@@ -34,9 +37,20 @@ public class SyncLecture {
             throw new RuntimeException(e);
         }
         System.out.println("Both tasks are done");
+        System.out.println("Count down ends");
     }
 
+
+/*    CyclicBarrier
+    Це синхронізаційний допоміжник, який дозволяє групі потоків чекати один одного, щоб досягти спільної бар'єрної точки.
+
+    Ініціалізація: Коли ви створюєте CyclicBarrier, ви вказуєте кількість потоків,
+    які повинні досягти бар'єрної точки, перш ніж потоки можуть продовжити виконання.
+    У нашому прикладі ми вказали 2, тому що у нас два потоки, які будуть досягати бар'єру.*/
+
+
     private static void cyclicBarrier(){
+        System.out.println("cyclicBarrier start");
         CyclicBarrier barrier = new CyclicBarrier(2, () -> {
             System.out.println("Both tasks reached the barrier");
         });
@@ -60,6 +74,45 @@ public class SyncLecture {
             }
             System.out.println("Task 2 passed the barrier");
         }).start();
+        System.out.println("cyclicBarrier ends");
+    }
+
+
+
+
+//    Semaphore
+//    Семафор контролює доступ до ресурсу за допомогою лічильника.
+//    Якщо лічильник більший за нуль, доступ дозволено; якщо він нульовий, доступ заборонено.
+
+    private static void semaphore() {
+        System.out.println("semaphore start");
+        Semaphore semaphore = new Semaphore(1);  // Тільки один потік може мати доступ одночасно
+
+        new Thread(() -> {
+            try {
+                semaphore.acquire();
+                System.out.println("Task 1 is running");
+                System.out.println("Thread 1 in semaphore ends");
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                semaphore.acquire();
+                System.out.println("Task 2 is running");
+                System.out.println("Thread 2 in semaphore ends");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }).start();
+        System.out.println("semaphore ends");
     }
 
 
