ea61f57c273f69d47f2211398764d6c240f0b3e1
Odyssey <vsakhanda@gmail.com>
2023-09-20 22:48:05 +0300

    homeWork 22

--- src/l22StreamAPIPart2/HomeWork21/Main.java
+++ src/l22StreamAPIPart2/HomeWork21/Main.java
@@ -8,46 +8,62 @@ package l22StreamAPIPart2.HomeWork21;
 2.1. Знайти суму цих чисел за допомогою Stream API
 */
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main {
 
 
-
     public static void main(String[] args) {
 
-        // 1.   Створити лист з 20 прізвищами. Вони повинні повторюватися.
-        List person = new ArrayList<>();
-        person.add("Oleksandr");
-        person.add("Serhii");
-        person.add("Alex");
-        person.add("Oleksandr");
-        person.add("Volodymyr");
-        person.add("Vladyslav");
-        person.add("Oksana");
-        person.add("Anna");
-        person.add("Maria");
-        person.add("Prokip");
-        person.add("Feofan");
-        person.add("Serhii");
-        person.add("Alex");
-        person.add("Oleksandr");
-        person.add("Volodymyr");
-        person.add("Vladyslav");
-        person.add("Oksana");
-        person.add("Anna");
-        person.add("Maria");
-        person.add("Prokip");
+        System.out.println("Завдання1. Список в Мапу ");
+        listToMap();
+        System.out.println("********");
+        System.out.println("Завдання2. Сума випадкових чисел");
+        randomNumbers();
+        System.out.println("********");
+    }
 
+    public static void listToMap() {
 
-    }}
-/*        1.2. За допомогою стрім апі перетворити цей лист на мапу.
+        List<String> surnameList = List.of(
+                "Апостол", "Благовісний", "Боголикий", "Боголюбськй", "Богонос", "Богославський",
+                "Богослов", "Богуслав", "Буян", "Великий", "Вернигора", "Галицький",
+                "Добриня", "Дубоніс", "Довгорукий", "Златоуст", "Калита", "Кандиба", "Кожем’яка", "Кривіша",
+                "Остромир", "Первозваний", "Пересвіт", "Печерський", "Победоносець", "Полуботко", "Порогост", "Преподобний", "Ягайло",
+                "Добриня", "Дубоніс", "Довгорукий", "Златоуст", "Калита", "Кандиба", "Кожем’яка", "Кривіша",
+                "Апостол", "Благовісний", "Боголикий", "Боголюбськй", "Богонос", "Богославський"
+        );
 
-        1.3. У якості ключа повинно бути прізвище, а я якості значення кількість повторень його.*/
-/*
-        Map<String, Map<String, Long>> collect = person
-                .stream()
-                .collect(Collectors.groupingBy(person.get(), Collectors.groupingBy(person::get(), Collectors.counting())));
+        System.out.println(grouping(surnameList));
+    }
 
-   }*/
+    static Map<String, Long> grouping(List<String> surnameList) {
+        return surnameList.stream().collect(Collectors.groupingBy(
+                        surname -> surname,
+                        Collectors.counting()
+                )
+        );
+    }
+
+    public static void randomNumbers() {
+        List<Integer> intList = List.of(
+                random(), random(), random(), random(), random(),
+                random(), random(), random(), random(), random()
+        );
+
+        System.out.println("List of integers: " + intList);
+        System.out.println("Sum of List of integers (first method) = " + sum(intList));
+    }
+
+    static Integer random() {
+        Random randomInt = new Random();
+        return randomInt.nextInt(10);
+    }
+
+    static Integer sum(List<Integer> list) {
+        return list.stream().mapToInt(Integer::intValue).sum();
+    }
+}
